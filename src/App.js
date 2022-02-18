38a9bae0324127bde527dff7437e0f5e3a69e3a4
mark-scholes <innominatus15@outlook.com>
2022-02-18 17:35:31 +0000

    Added Axios and created api call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios';
 
 function App() {
+
+  const [coinData, setCoinData] = useState([])
+  useEffect(() => {
+    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+    axios.get(url)
+    .then(res => {
+      setCoinData(res.data)
+    }).catch(error => console.log(error));
+  },[])
+
   return (
     <div className="App">
-      
+      Crypto
     </div>
   );
 }
