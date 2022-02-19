e8122ae393e22cfdf727e51260659718e452589f
mark-scholes <innominatus15@outlook.com>
2022-02-19 03:47:38 +0000

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -6,34 +6,46 @@ import Coin from './components/Coin/Coin';
 function App() {
 
   const [coinData, setCoinData] = useState([])
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false';
     axios.get(url)
     .then(res => {
       setCoinData(res.data)
+      filerCoins()
     }).catch(error => console.log(error));
   },[])
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+  const filerCoins = coinData.filter(coin => 
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  )
 
   return (
     <div className="App">
     <div className="coin-search">
     <h1 className="coin-text">Search for a coin</h1>
     <form>
-      <input type="text" name="" id="" className="coin-search" placeholder='Search' />
+      <input type="text" name="" id="" className="coin-search" placeholder='Search' value={search} onChange={handleChange} />
     </form>
     <div className="coins cointainer" style={{display: "flex", flexDirection: "column" }}>
-    { coinData.length > 0 ? coinData.map((coin)=> (
+    
+    { filerCoins.map((coin)=> (
       <Coin 
         img={coin.image}
         name={coin.name}
-        key={coin.id}
+        key={coin.id} 
         symbol={coin.symbol}
         current_price={coin.current_price.toLocaleString('en-UK', { style: 'currency', currency: 'GBP' })}
         volume={coin.total_volume.toLocaleString()}
         price_change_24h={coin.price_change_24h.toLocaleString('en-uk', {style: "currency", currency: "GBP"})}
+        price_change_percentage_24h={coin.price_change_percentage_24h}
         />
-    )) : ""}
+    ))}
     </div>
     
     
--- src/components/Coin/Coin.js
+++ src/components/Coin/Coin.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-const Coin = ({img , name, symbol, current_price, volume, price_change_24h}) => {
+const Coin = ({img , name, symbol, current_price, volume, price_change_24h, price_change_percentage_24h}) => {
   return (
     <div className="coin-container">
         <div className="coin-row" >
@@ -12,6 +12,13 @@ const Coin = ({img , name, symbol, current_price, volume, price_change_24h}) => 
                 <p className="coin-currentPrice">{current_price}</p>
                 <p className="coin-volume">{volume}</p>
                 <p className="coin-priceChange">{price_change_24h}</p>
+                {
+                  price_change_percentage_24h < 0 ? (
+                    <p className="coin-percentRed">{price_change_percentage_24h.toFixed(2)}%</p>
+                  ) : (
+                    <p className="coin-percentGreen">{price_change_percentage_24h.toFixed(2)}%</p>
+                  )
+                }
             </div>
         </div>
     </div>
